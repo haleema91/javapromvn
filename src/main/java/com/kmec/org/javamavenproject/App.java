915fab7e297c60f806d713e8bdac2f80f57ce2d3
Haleema Bushra <cutebushra8@gmail.com>
2024-11-16 11:11:41 +0530

    adding mvn pro

---
+++ src/main/java/com/kmec/org/javamavenproject/App.java
@@ -0,0 +1,13 @@
+package com.kmec.org.javamavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/kmec/org/javamavenproject/AppTest.java
@@ -0,0 +1,20 @@
+package com.kmec.org.javamavenproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
